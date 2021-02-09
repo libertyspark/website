da3189dbad42ea8c2c970d6ab8fe11e0ad3e91b1
jason <jason@bullard.io>
2021-02-09 13:40:45 +0000

    removed old menu close button

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,12 +1,4 @@
-document.getElementById('burgerBtn').addEventListener('click', () => {
-  toggleMobileMenu()
-});
-
-document.getElementById('mobileNavCloseBtn').addEventListener('click', () => {
-  toggleMobileMenu();
-})
-
 // show/hide the mobile menu when the burgerBtn is clicked
-function toggleMobileMenu() {
+document.getElementById('burgerBtn').addEventListener('click', () => {
   document.getElementById('mobileMenu').classList.toggle('hidden');
-}
+});
